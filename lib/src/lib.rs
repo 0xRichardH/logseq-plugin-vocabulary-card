91c4bd5382dc4110a46f0c7cc82dcd38ec0aa17d
Richard Hao <richard@0xdev.dev>
2024-01-03 23:20:10 +0800

    refactor: extract make_request and extract_dictionary functions in rust

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -22,8 +22,6 @@ pub async fn define_word(word: &str, api_key: &str) -> Result<JsValue, JsValue> 
         "please act as a dictionary, including the pronunciation, explanation, two examples of sentences, and one image. And the word is {}. please output the result in json format, the json keys are `word`, `pronunciation`, `definition`, `examples`, and `image`.",
         word.trim()
     );
-
-    let req_client = reqwest::Client::new();
     let req_body = serde_json::json!({
         "contents": [{
             "parts": [{
@@ -33,32 +31,43 @@ pub async fn define_word(word: &str, api_key: &str) -> Result<JsValue, JsValue> 
 
     });
 
+    let res_body = make_request(&url, req_body)
+        .await
+        .map_err(|e| JsValue::from_str(&e))?;
+    let dictionary = extract_dictionary(res_body).map_err(|e| JsValue::from_str(&e))?;
+
+    Ok(serde_wasm_bindgen::to_value(&dictionary)?)
+}
+
+async fn make_request(url: &str, body: serde_json::Value) -> Result<serde_json::Value, String> {
+    let req_client = reqwest::Client::new();
     let res = req_client
         .post(url)
-        .json(&req_body)
+        .json(&body)
         .send()
         .await
-        .map_err(|e| JsValue::from_str(&format!("request error: {}", e)))?;
+        .map_err(|e| format!("request error: {}", e))?;
     if res.status() != StatusCode::OK {
-        return Err(JsValue::from_str(&format!(
-            "request error: {}",
-            res.status()
-        )));
+        return Err(format!("request error: {}", res.status()));
     }
     let json = res
         .json::<serde_json::Value>()
         .await
-        .map_err(|e| JsValue::from_str(&format!("json error: {}", e)))?;
-    let Some(dictionary_str) = json["candidates"][0]["content"]["parts"][0]["text"].as_str() else {
-        return Err(JsValue::from_str("json error: dictionary not found"));
+        .map_err(|e| format!("json error: {}", e))?;
+    Ok(json)
+}
+
+fn extract_dictionary(res_body: serde_json::Value) -> Result<Word, String> {
+    let Some(dictionary_str) = res_body["candidates"][0]["content"]["parts"][0]["text"].as_str()
+    else {
+        return Err("json error: dictionary not found".to_string());
     };
     let dictionary_str = dictionary_str
         .trim_start_matches("```json")
         .trim_start_matches("```")
         .trim_end_matches("```")
         .trim();
-    let dictionary = serde_json::from_str::<Word>(dictionary_str)
-        .map_err(|e| JsValue::from_str(&format!("serde error: {}", e)))?;
-
-    Ok(serde_wasm_bindgen::to_value(&dictionary)?)
+    let dictionary =
+        serde_json::from_str::<Word>(dictionary_str).map_err(|e| format!("serde error: {}", e))?;
+    Ok(dictionary)
 }
